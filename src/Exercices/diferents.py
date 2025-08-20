6ae42a199b46d0b3e92a0ccc187fe505454266bf
VitorRLK1 <vitinhodmt@hotmail.com>
2025-08-19 21:52:49 -0300

    first commit

---
+++ src/Exercices/diferents.py
@@ -0,0 +1,9 @@
+Genre = str(input('Se for homem put M se for mulher put F'))
+if Genre.upper() == "M":
+    print('Homem')
+
+elif Genre.upper() == "F":
+    print('Mulher')
+
+else : 
+    print('Genre invalid')
---
+++ src/Exercices/ifelse.py
@@ -0,0 +1,8 @@
+value = int(input())
+if value > 20: 
+    print('Maior')
+elif value < 5 and value < 10:
+    print('Menor')
+
+else:
+    print('Value Invalid')
