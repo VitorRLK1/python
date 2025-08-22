abaead88abaeb2ecb043ca6bc9cb2bf1e832046d
VitorRLK1 <vitinhodmt@hotmail.com>
2025-08-21 21:45:33 -0300

    aula python

---
+++ src/Exercices/aula.py
@@ -0,0 +1,16 @@
+nome_completo = 'Maria da Silva'
+idade = 37
+peso = 68.45
+altura = 1.65
+quantidade_de_irmaos = 3
+quantidade_de_celulares = 2
+marca = 'Chevrolet'
+modelo = 'Onix'
+nome_do_cachorro = 'Marley'
+
+print('Nome: ', nome_completo, 'Idade: ', idade)
+print('Peso: ', peso, 'Altura: ' , altura)
+print(quantidade_de_irmaos, 'irmãos')
+print(quantidade_de_celulares, 'celulares')
+print('Marca: ', marca, 'Modelo: ', modelo)
+print('Nome do cachorro: ', nome_do_cachorro)
